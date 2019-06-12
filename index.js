c658a0ab759a10b047a259e7022484f29807a9df
oluwatobi balogun <tobibags19@gmail.com>
2019-06-12 00:35:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var katzDeli = []
 
-function takeANumber(katzDeli, name) {
-  katzDeli.push(`${name}`)
-  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`)
+ function takeANumber(katzDeli, name) {
+  katzDeli.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
 
-function nowServing(katzDeli) {
+
+ function nowServing(katzDeli) {
   let i = 0;
   while (i < katzDeli.length) {
     i++;
@@ -17,15 +17,16 @@ function nowServing(katzDeli) {
   return (`Currently serving ${katzDeli.shift()}.`);
 }
 
+
+
  var line = [];
 
  function currentLine(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
+  for (let i = 0; i < katzDeli.length; i++) {
     line.push(` `+[i+1]+`. `  + katzDeli[i])
-    i++;
   }
   if (katzDeli.length === 0) {
     return "The line is currently empty.";
   } else
   return(`The line is currently:` + line);
+ }
