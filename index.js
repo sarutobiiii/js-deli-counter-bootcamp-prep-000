14151449cbb446d98894cc206b03aa4ec51373ef
oluwatobi balogun <tobibags19@gmail.com>
2019-06-12 00:32:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,18 +18,15 @@ function nowServing(katzDeli) {
   }
 }
 
-var line = []
+ var line = [];
 
-function currentLine(katzDeli) {
-  let i = 0
+ function currentLine(katzDeli) {
+  let i = 0;
   while (i < katzDeli.length) {
-    line.push(` ` + [i+1] + `. ` + katzDeli[i])
-    i++
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
+    i++;
   }
   if (katzDeli.length === 0) {
-    return ("The line is currently empty.")
-  }
-  else {
-    return (`The line is currently:` + line)
-  }
-}
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
